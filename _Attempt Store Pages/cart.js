47e19e9dc4814c6cd3c4960f8e44bd131457118b
lounder <tornadobg123@gmail.com>
2021-09-06 00:05:42 +0300

    Added store pages attempt files

---
+++ _Attempt Store Pages/cart.js
@@ -0,0 +1,9 @@
+// function name() {
+//     var page1 = document.getElementById('page1');
+//     var page2 = document.getElementById('page2');
+
+//     var goToPage1 = document.getElementById('goToPage1');
+//     var goToPage2 = document.getElementById('goToPage2');
+
+//     goToPage1.onclick = function () { alert('hello!'); };
+//   }
