149e9980eda7c6fad6be07f2fe1c8fc49227bc6e
lounder <tornadobg123@gmail.com>
2021-09-14 15:01:48 +0300

    Git ignore tweaks

--- _Attempt Store Pages/cart.js
+++
@@ -1,9 +0,0 @@
-// function name() {
-//     var page1 = document.getElementById('page1');
-//     var page2 = document.getElementById('page2');
-
-//     var goToPage1 = document.getElementById('goToPage1');
-//     var goToPage2 = document.getElementById('goToPage2');
-
-//     goToPage1.onclick = function () { alert('hello!'); };
-//   }
