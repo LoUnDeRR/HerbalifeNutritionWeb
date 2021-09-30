e0ef865285384d2c37ea43f9a096782d6a31846d
lounder <tornadobg123@gmail.com>
2021-10-01 00:54:12 +0300

    Fixed Total Price and Added Pages

--- products.js
+++ products.js
@@ -6,7 +6,7 @@ const products = [
     name : "NAME IS WORKING",
     description : "DESC IS WORKING",
     images:["dummy-pdt-b.png", "dummy-pdt-a.jpg", "p1.png", "p2.jpg", "p3.jpg"],
-    price : 99.99,
+    price : 10.00,
     new : true
   },
   {
@@ -14,7 +14,7 @@ const products = [
     name : "NAME2 IS WORKING",
     description : "DESC IS WORKING",
     images:["p4.jpg", "p2.jpg"],
-    price : 98.99,
+    price : 1.99,
     new : false
   },
   {
@@ -22,7 +22,7 @@ const products = [
     name : "NAME IS WORKING",
     description : "DESC IS WORKING",
     images:["dummy-pdt-b.png", "dummy-pdt-a.jpg", "p1.png", "p2.jpg", "p3.jpg"],
-    price : 99.99,
+    price : 10.00,
     new : true
   },
   {
@@ -30,7 +30,151 @@ const products = [
     name : "NAME IS WORKING",
     description : "DESC IS WORKING",
     images:["dummy-pdt-b.png", "dummy-pdt-a.jpg", "p1.png", "p2.jpg", "p3.jpg"],
-    price : 99.99,
+    price : 10.00,
     new : true
+  },
+  {
+    id: 123,
+    name : "NAME IS WORKING",
+    description : "DESC IS WORKING",
+    images:["dummy-pdt-b.png", "dummy-pdt-a.jpg", "p1.png", "p2.jpg", "p3.jpg"],
+    price : 10.00,
+    new : true
+  },
+  {
+    id: 124,
+    name : "NAME2 IS WORKING",
+    description : "DESC IS WORKING",
+    images:["p4.jpg", "p2.jpg"],
+    price : 1.99,
+    new : false
+  },
+  {
+    id: 123,
+    name : "NAME IS WORKING",
+    description : "DESC IS WORKING",
+    images:["dummy-pdt-b.png", "dummy-pdt-a.jpg", "p1.png", "p2.jpg", "p3.jpg"],
+    price : 10.00,
+    new : true
+  },
+  {
+    id: 124,
+    name : "NAME2 IS WORKING",
+    description : "DESC IS WORKING",
+    images:["p4.jpg", "p2.jpg"],
+    price : 1.99,
+    new : false
+  },
+  {
+    id: 124,
+    name : "NAME2 IS WORKING",
+    description : "DESC IS WORKING",
+    images:["p4.jpg", "p2.jpg"],
+    price : 1.99,
+    new : false
+  },
+  {
+    id: 124,
+    name : "NAME2 IS WORKING",
+    description : "DESC IS WORKING",
+    images:["p4.jpg", "p2.jpg"],
+    price : 1.99,
+    new : false
+  },
+  {
+    id: 123,
+    name : "NAME IS WORKING",
+    description : "DESC IS WORKING",
+    images:["dummy-pdt-b.png", "dummy-pdt-a.jpg", "p1.png", "p2.jpg", "p3.jpg"],
+    price : 10.00,
+    new : true
+  },
+  {
+    id: 124,
+    name : "NAME2 IS WORKING",
+    description : "DESC IS WORKING",
+    images:["p4.jpg", "p2.jpg"],
+    price : 1.99,
+    new : false
+  },
+  {
+    id: 124,
+    name : "NAME2 IS WORKING",
+    description : "DESC IS WORKING",
+    images:["p4.jpg", "p2.jpg"],
+    price : 1.99,
+    new : false
+  },
+  {
+    id: 124,
+    name : "NAME2 IS WORKING",
+    description : "DESC IS WORKING",
+    images:["p4.jpg", "p2.jpg"],
+    price : 1.99,
+    new : false
+  },
+  {
+    id: 124,
+    name : "NAME2 IS WORKING",
+    description : "DESC IS WORKING",
+    images:["p4.jpg", "p2.jpg"],
+    price : 1.99,
+    new : false
+  },
+  {
+    id: 124,
+    name : "NAME2 IS WORKING",
+    description : "DESC IS WORKING",
+    images:["p4.jpg", "p2.jpg"],
+    price : 1.99,
+    new : false
+  },
+  {
+    id: 124,
+    name : "NAME2 IS WORKING",
+    description : "DESC IS WORKING",
+    images:["p4.jpg", "p2.jpg"],
+    price : 1.99,
+    new : false
+  },
+  {
+    id: 123,
+    name : "NAME IS WORKING",
+    description : "DESC IS WORKING",
+    images:["dummy-pdt-b.png", "dummy-pdt-a.jpg", "p1.png", "p2.jpg", "p3.jpg"],
+    price : 10.00,
+    new : true
+  },
+  {
+    id: 124,
+    name : "NAME2 IS WORKING",
+    description : "DESC IS WORKING",
+    images:["p4.jpg", "p2.jpg"],
+    price : 1.99,
+    new : false
+  },
+  {
+    id: 124,
+    name : "NAME2 IS WORKING",
+    description : "DESC IS WORKING",
+    images:["p4.jpg", "p2.jpg"],
+    price : 1.99,
+    new : false
+  },
+  {
+    id: 124,
+    name : "NAME2 IS WORKING",
+    description : "DESC IS WORKING",
+    images:["p4.jpg", "p2.jpg"],
+    price : 1.99,
+    new : false
+  },
+  {
+    id: 124,
+    name : "NAME2 IS WORKING",
+    description : "DESC IS WORKING",
+    images:["p4.jpg", "p2.jpg"],
+    price : 1.99,
+    new : false
   }
 ]
