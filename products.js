160b9a9e1aa1dfc82de196f200737528612c136e
lounder <tornadobg123@gmail.com>
2021-10-16 00:55:03 +0300

    mobile product overlay

--- products.js
+++ products.js
@@ -4,7 +4,7 @@ const products = [
   {
     id: 123,
     name : "NAME IS WORKING",
-    description : "DESC IS WORKING",
+    description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
     images:["dummy-pdt-b.png", "dummy-pdt-a.jpg", "p1.png", "p2.jpg", "p3.jpg"],
     price : 10.00,
     new : true
